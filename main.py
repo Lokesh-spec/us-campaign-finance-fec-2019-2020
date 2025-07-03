cef10fc46c9235d26791e2e93e0c91f4689f19a8
Lokesh K V <63384554+Lokesh-spec@users.noreply.github.com>
2025-07-03 19:10:17 +0000

    Added the function to upload files to GCS Bucket in main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import yaml
 from pprint import pprint
 
-from src.filessio_uploader import need_to_name  
-
-
-
-
-
-
-
-
+from src.filessio_uploader import upload_fec_files_to_mysql
+from src.gcs_uploader import upload_fec_files_to_gcs
 
 
 if __name__ == "__main__":
@@ -22,15 +15,12 @@ if __name__ == "__main__":
         except yaml.YAMLError as exc:
             print(f"Error in configuration file: {exc}")
             exit(1)
+
+    try:
+        upload_fec_files_to_mysql(config)
+        upload_fec_files_to_gcs(config)
+    except Exception as e:
+        print(f"An error occurred while uploading files to MySQL and GCS bucket: {e}")
+        exit(1)
+
     
-    # Transform and Uploads data from a CSV file to the specified MySQL table.
-    host = config['mysql']['host']
-    port = config['mysql']['port']
-    username = config['mysql']['username']
-    password = config['mysql']['password']
-    database = config['mysql']['database']
-
-    # pprint(config)
-
-    need_to_name(config)
-
